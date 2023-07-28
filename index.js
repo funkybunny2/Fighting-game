17a9f64fe3fc0596e75b9e7128353dd00ce7fe05
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-07-28 13:30:59 -0500

    changed color

--- index.js
+++ index.js
@@ -8,17 +8,27 @@ c.fillRect(0,0,canvas.width, canvas.height);
 const gravity = 0.1;
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = 'red'}) {
         this.position = position;
         this.velocity = velocity;
         this.height = 150;
         this.weight = 50;
         this.lastKey;
+        this.attackBox = {
+            position: this.position ,
+            width: 100,
+            height: 50
+        }
+        this.color = color;
     }
 
     draw() {
-        c.fillStyle = 'red';
+        c.fillStyle = this.color;
         c.fillRect(this.position.x,this.position.y, 50, 150 )
+
+        //attack box
+        c.fillStyle = 'white'
+        c.fillRect(this.attackBox.position.x,this.attackBox.position.y,this.attackBox.width,this.attackBox.height);
     }
 
     update() {
@@ -39,7 +49,7 @@ const player = new Sprite({position:{x:0, y:0 },velocity: {x:0, y:0} });
 
 player.draw();
 
-const enemy = new Sprite({position:{x:400, y:100 },velocity: {x:0, y:0} });
+const enemy = new Sprite({position:{x:400, y:100 },velocity: {x:0, y:0} ,color:'blue'});
 
 enemy.draw();
 
